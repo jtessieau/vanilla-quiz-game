165da08683e6dad916c9c0e8d752d60c44d212ef
Julien TESSIEAU <julien.tessieau@gmail.com>
2024-03-03 14:21:12 +0000

    add check answer functionnality

--- app/quiz.js
+++ app/quiz.js
@@ -34,6 +34,19 @@ function update() {
     displayAnswers();
 }
 
+function nextQuestion() {
+    currentQuestion = selectRandomQuestion(questionsArray);
+
+    nextButton.onclick = null;
+    nextButton.disabled = true;
+
+    if (currentQuestion !== null) {
+        update();
+    } else {
+        alert('No more questions! please reset the game.');
+    }
+}
+
 function displayAnswers() {
     answersContainer.innerHTML = '';
 
@@ -52,12 +65,43 @@ function displayAnswers() {
     Object.values(answersArray).forEach((value) => {
         const btn = document.createElement('button');
         btn.setAttribute('type', 'button');
+        btn.setAttribute('data-answer', value);
         btn.innerHTML = value;
         btn.onclick = (event) => checkAnswer(event);
         answersContainer.appendChild(btn);
     });
 }
 
-function checkAnswer(event) {}
+function checkAnswer(event) {
+    const answer = event.target.getAttribute('data-answer');
+    if (answer === currentQuestion.correct_answer) {
+        event.target.style.backgroundColor = 'green';
+        event.target.onclick = null;
+        event.target.setAttribute('data-guess', 'right');
+
+        nextButton.disabled = false;
+        nextButton.onclick = nextQuestion;
+
+        const buttons = answersContainer.children;
+
+        Object.values(buttons).forEach((element) => {
+            if (
+                element.tagName === 'BUTTON' &&
+                element.getAttribute('data-guess') !== 'right'
+            ) {
+                element.disabled = true;
+            }
+        });
+        currentQuestion.answered = true;
+        questionsArray = removeAnsweredQuestion(questionsArray);
+    } else {
+        event.target.style.backgroundColor = 'red';
+        event.target.onclick = null;
+    }
+}
+
+function removeAnsweredQuestion(questions) {
+    return questions.filter((question) => question.answered !== true);
+}
 
 initGame();
